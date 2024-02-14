6c2bdfe48a0780a41d3f4ddabfd227e98e2c29c6
Ab03beed <abdaher2000@gmail.com>
2024-02-14 14:42:59 +0100

    Resturcted the code

--- app/src/main/java/se/miun/dt170g/waiterapp/NewOrder.java
+++ app/src/main/java/se/miun/dt170g/waiterapp/NewOrder.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 ;
 
@@ -37,16 +38,23 @@ public class NewOrder extends AppCompatActivity {
         setUpOrderInput();
 
         InputAdapter adapter = new InputAdapter(this, inputModels);
+
         recyclerView1.setAdapter(adapter);
         recyclerView1.setLayoutManager(new LinearLayoutManager(this));
 
-        //InputAdapter adapter = new InputAdapter(this, inputModels);
+
         recyclerView2.setAdapter(adapter);
         recyclerView2.setLayoutManager(new LinearLayoutManager(this));
 
-        //InputAdapter adapter = new InputAdapter(this, inputModels);
+
         recyclerView3.setAdapter(adapter);
         recyclerView3.setLayoutManager(new LinearLayoutManager(this));
+
+        NumberPicker numberPicker = findViewById(R.id.itemCount);
+
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(100);
+
 
 
     }
